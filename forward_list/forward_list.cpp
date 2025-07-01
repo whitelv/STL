4b58e0fa2e1ef54fce6698705f227ab0900032a6
whitelv <lubomyr0901@gmail.com>
2025-07-01 16:23:54 +0300

    forward_list

---
+++ forward_list/forward_list.cpp
@@ -0,0 +1,66 @@
+//
+//  array.cpp
+//  STL
+//
+//  Created by Любомир Витвицький on 24.06.2025.
+//
+
+#include <iostream>
+#include <forward_list>
+#include "forward_list.h"
+
+template <typename T>
+struct Node{
+    T value;
+    Node * next;
+};
+
+template <typename T>
+void display(const std::forward_list<T> & a){
+    for (const int &x : a) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main(){
+    std::cout << "-------STL class:--------" << std::endl;
+    
+    std::forward_list<int> a{1,2,3,4,5};
+    std::forward_list<int> b{1,2,7,4,5};
+    
+    a.merge(b);
+    
+    
+    display(a);
+    
+    
+    std::cout << "-------------------------" << std::endl;
+    
+    
+    std::cout << "------Custom class:------" << std::endl;
+    
+    
+    
+    Forward_list<int> f1 {4,1,24};
+    
+    std::vector p {1,2,3,45,6,788,90,9,9};
+    
+    Forward_list<int> f2 {7,5,7,7,7};
+    
+    
+    
+
+
+    f2.remove(7);
+    
+    f2.display();
+
+    return 0;
+    
+    
+    
+    
+
+}
---
+++ forward_list/forward_list.h
@@ -0,0 +1,528 @@
+
+
+#ifndef FORWARD_LIST_H
+#define FORWARD_LIST_H
+
+#include <initializer_list>
+
+
+
+template <typename T>
+class Forward_list{
+    
+private:
+    struct Node{
+        T value;
+        Node * next;
+    };
+    
+    class forward_iterator{
+    friend class Forward_list<T>;
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = T;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = T*;
+        using reference         = T&;
+
+        
+        forward_iterator() : node(nullptr){};
+        explicit forward_iterator(Node * n) : node(n){};
+        forward_iterator(const forward_iterator & node) = default;
+        forward_iterator & operator=(const forward_iterator & node) = default;
+
+        ~forward_iterator(){}
+        
+        
+        bool operator==(const forward_iterator& other) const = default;
+
+        T & operator*(){return node->value;}
+        const T & operator*() const{return node->value;}
+        
+        
+        
+        T * operator->(){return &node->value;}
+        const T * operator->() const {return &node->value;}
+
+        forward_iterator & operator++(){
+            node = node->next;
+            return *this;
+        }
+        forward_iterator operator++(int){
+            forward_iterator temp = *this;
+            node = node->next;
+            return temp;
+        }
+                
+    private:
+        Node * node;
+    };
+    
+    
+    
+public:
+    void display(){
+        Node * temp = head;
+        while(temp != nullptr){
+            std::cout << temp->value << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+    
+// Constructors
+public:
+    Forward_list(): head(nullptr){};
+    
+    Forward_list(size_t count, const T& value) : head(nullptr){
+        if (count == 0) {
+            return;
+        }
+        fill(count, value);
+    }
+    
+    template<std::input_iterator InputIt>
+    Forward_list(InputIt first, InputIt last) : head(nullptr){
+        if (first == last) {
+            return;
+        }
+        copy_values(first, last);
+        
+    }
+    
+    Forward_list(const Forward_list & other) : head(nullptr){
+        if (other.head == nullptr) {
+            return;
+        }
+        copy_values(other);
+    }
+    
+    Forward_list(Forward_list && other) : head(other.head){
+        other.head = nullptr;
+    }
+    
+    Forward_list(const std::initializer_list<T> & ilist) : head(nullptr){
+        if (ilist.size() == 0) {
+            return;
+        }
+        copy_values(ilist);
+    }
+    
+    ~Forward_list(){
+        clear();
+        
+    }
+    
+    
+    // assignment operator
+public:
+    Forward_list & operator=( const Forward_list& other ){
+        if (this == &other) {
+            return * this;
+        }
+        clear();
+        
+        if (other.head == nullptr) {
+            return * this;
+        }
+        copy_values(other);
+        
+        return *this;
+        
+    }
+    
+    Forward_list & operator=(Forward_list && other ){
+        if (this == &other) {
+            return * this;
+        }
+        clear();
+        
+        head = other.head;
+        other.head = nullptr;
+        return * this;
+    }
+    
+    // assign
+public:
+    void assign(size_t count, const T& value ){
+        clear();
+        if (count == 0) {
+            return;
+        }
+        fill(count, value);
+    }
+    
+    template<std::input_iterator InputIt >
+    void assign(InputIt first, InputIt last ){
+        clear();
+        if (first == last){
+            return;
+        }
+        copy_values(first, last);
+    }
+    
+    void assign( std::initializer_list<T> ilist ){
+        clear();
+        if (ilist.size() == 0) {
+            return;
+        }
+        copy_values(ilist);
+    }
+    
+    
+// Element access
+public:
+    T & front(){
+        return head;
+    }
+    
+    const T & front() const{
+        return head;
+    }
+    
+//Capacity
+public:
+    bool empty() const noexcept{
+        return head == nullptr;
+    }
+    
+    forward_iterator insert_after(forward_iterator pos, const T& value ){
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot insert after end()");
+        }
+        Node * new_node = create_new_node(value);
+        Node * next = pos.node->next;
+        pos.node->next = new_node;
+        new_node->next = next;
+        return forward_iterator(new_node);
+    }
+    
+    forward_iterator insert_after(forward_iterator pos, T&& value ){
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot insert after end()");
+        }
+        Node * new_node = create_new_node(std::move(value));
+        Node * next = pos.node->next;
+        pos.node->next = new_node;
+        new_node->next = next;
+        return forward_iterator(new_node);
+    }
+    
+    forward_iterator insert_after(forward_iterator pos, size_t count, const T& value ){
+        if (count == 0) {
+            return pos;
+        }
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot insert after end()");
+        }
+        
+        Node * temp_head = create_new_node(value);
+        Node * temp_tail = temp_head;
+        
+        for (size_t i = 0; i < count - 1; i++) {
+            Node* new_node = create_new_node(value);
+            temp_tail->next = new_node;
+            temp_tail = new_node;
+        }
+        
+        Node * next = pos.node->next;
+        pos.node->next = temp_head;
+        temp_tail->next = next;
+        
+        return forward_iterator(temp_tail);
+    }
+    
+    
+    template< std::input_iterator InputIt >
+    forward_iterator insert_after(forward_iterator pos,InputIt first, InputIt last ){
+        if (first == last) {
+            return pos;
+        }
+        
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot insert after end()");
+        }
+        
+        Node * temp_head = create_new_node(*first++);
+        Node * temp_tail = temp_head;
+        
+        while (first != last) {
+            Node* new_node = create_new_node(*first++);
+            temp_tail->next = new_node;
+            temp_tail = new_node;
+        }
+        
+        Node * next = pos.node->next;
+        pos.node->next = temp_head;
+        temp_tail->next = next;
+    
+        return forward_iterator(temp_tail);
+    }
+    
+    
+    forward_iterator insert_after(forward_iterator pos, std::initializer_list<T> ilist ){
+        if (ilist.size() == 0) {
+            return pos;
+        }
+        
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot insert after end()");
+        }
+        
+        auto it = ilist.begin();
+        
+        Node * temp_head = create_new_node(*it++);
+        Node * temp_tail = temp_head;
+        
+        while (it != ilist.end()) {
+            Node* new_node = create_new_node(*it++);
+            temp_tail->next = new_node;
+            temp_tail = new_node;
+        }
+        
+        Node * next = pos.node->next;
+        pos.node->next = temp_head;
+        temp_tail->next = next;
+    
+        return forward_iterator(temp_tail);
+    }
+    
+    template< class... Args >
+    forward_iterator emplace_after(forward_iterator pos, Args&&... args ){
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot insert after end()");
+        }
+        
+        
+        Node * new_node = new Node(std::forward<Args>(args)...);
+        Node * next = pos.node->next;
+        pos.node->next = new_node;
+        new_node->next = next;
+        
+
+        return forward_iterator(new_node);
+    }
+    
+    forward_iterator erase_after(forward_iterator pos ){
+        if (pos.node == nullptr) {
+            throw std::invalid_argument("Cannot erase after end()");
+        }
+        if (pos.node->next == nullptr) {
+            return forward_iterator(nullptr);
+        }
+        
+        if (pos.node->next == head) {
+            pop_front();
+            return forward_iterator(head);
+        }
+        
+        Node * erase_element = pos.node->next;
+        Node * next_after_erased = erase_element->next;
+        delete erase_element;
+        pos.node->next = next_after_erased;
+        return forward_iterator(next_after_erased);
+    }
+    
+    forward_iterator erase_after(forward_iterator first, forward_iterator last ){
+        if (first == last) {
+            return last;
+        }
+        if (first.node == nullptr) {
+            throw std::invalid_argument("Cannot erase after end()");
+        }
+        
+        Node * current = first.node->next;
+        while (current != last.node) {
+            Node * next = current->next;
+            delete current;
+            current = next;
+        }
+        first.node->next = last.node;
+        return last;
+    }
+    
+    void push_front( const T& value ){
+        Node * new_head = create_new_node(value);
+        new_head->next = head;
+        head = new_head;
+    }
+    
+    void push_front( T&& value ){
+        Node * new_head = create_new_node(std::move(value));
+        new_head->next = head;
+        head = new_head;
+    }
+    
+    template< class... Args >
+    T & emplace_front( Args&&... args ){
+        Node * new_head = new Node(std::forward<Args>(args)...);
+        new_head->next = head;
+        head = new_head;
+        return head->value;
+    }
+    
+    void pop_front(){
+        if (empty()) {
+            return;
+        }
+        Node * next = head->next;
+        delete head;
+        head = next;
+    }
+    
+    void resize( size_t count , const T & value  = T()){
+        
+        if (count == 0) {
+            clear();
+            return;
+        }
+        
+        if (!head) {
+            head = create_new_node(value);
+            insert_after(forward_iterator(head), count - 1, value);
+            return;
+        }
+        
+        size_t i = 1;
+        Node * current = head;
+        
+        while (current->next != nullptr && i< count) {
+            current = current->next;
+            i++;
+        }
+        if (current->next != nullptr) {
+            erase_after(forward_iterator(current), end());
+        }else{
+            insert_after(forward_iterator(current), count - i, value);
+        }
+    }
+    
+
+    
+    
+    void swap(Forward_list& other ){
+        Node * temp = other.head;
+        other.head = head;
+        head = temp;
+    }
+    
+    
+    void remove( const T& value ){
+        if(head == nullptr){
+            return;
+        }
+        
+        Node * fake_head = new Node(0, head);
+        Node * current = fake_head;
+        
+        
+        while (current != nullptr && current->next != nullptr) {
+            if(current->next->value == value){
+                std::cout << head->value << std::endl;
+                erase_after(forward_iterator(current));
+                std::cout << head->value << std::endl;
+                display();
+            }else{
+                current = current->next;
+                std::cout << current->next << std::endl;
+            }
+        }
+    }
+
+
+    
+    
+// Iterators
+public:
+    forward_iterator begin() noexcept{
+        return forward_iterator(head);
+    }
+    
+    forward_iterator end() noexcept{
+        return forward_iterator(nullptr);
+    }
+    
+    
+// Modifiers
+public:
+    void clear() noexcept{
+        Node * current = head;
+        while (current != nullptr) {
+            Node * next = current->next;
+            delete current;
+            current = next;
+        }
+        head = nullptr;
+    }
+    
+
+private:
+    
+    
+    Node * create_new_node(const T & value){
+        return new Node{value, nullptr};
+    }
+    
+    Node* create_new_node(T&& value) {
+        return new Node(std::move(value));
+    }
+    
+    void fill(size_t count, const T & value){
+        for (size_t i = 0; i < count; i++) {
+            Node * new_head = create_new_node(value);
+            new_head->next = head;
+            head = new_head;
+        }
+    }
+    
+    void copy_values(const Forward_list & other){
+        
+        Node * temp =other.head;
+        
+        // create head
+        head = create_new_node(temp->value);
+        Node * tail = head;
+        temp = temp->next;
+        
+        
+        while (temp != nullptr) {
+            Node * new_tail = create_new_node(temp->value);
+            tail->next = new_tail;
+            tail = new_tail;
+            temp = temp->next;
+        }
+    }
+    
+    template<std::input_iterator InputIt>
+    void copy_values(InputIt first, InputIt last){
+        head = create_new_node(*first++);
+        Node * tail = head;
+        while (first != last) {
+            Node * new_tail = create_new_node(*first++);
+            tail->next = new_tail;
+            tail = new_tail;
+        }
+    }
+    
+    void copy_values(const std::initializer_list<T> & ilist){
+        auto it = ilist.begin();
+        
+        head = create_new_node(*it++);
+        Node * tail = head;
+        
+        while (it != ilist.end()) {
+            Node * new_tail = create_new_node(*it++);
+            tail->next = new_tail;
+            tail = new_tail;
+        }
+    }
+    
+
+private:
+    Node * head;
+    
+    
+};
+
+
+#endif

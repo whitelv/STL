451f8bab3967ebd177be1ebbeaf1686bddda0f9b
whitelv <lubomyr0901@gmail.com>
2025-07-03 18:12:32 +0300

    update list

--- list/list.cpp
+++ list/list.cpp
@@ -1,6 +1,7 @@
 #include "list.h"
 #include <list>
 #include <iostream>
+#include <vector>
 
 
 int main(){
@@ -11,16 +12,33 @@ int main(){
     std::cout << "----------------" << std::endl;
     std::cout << "doubly-linked list:" << std::endl;
 
-    
 
 
-    List<int> l2 {1,2,5,6,7};
-// 
-    List<int> l1 {l2.begin(), l2.end()};
+    List<int> l2 {1,2,3};
 
-    l1.clear();
+
+
+    List<int> l1(l2.begin(), l2.end());
+
+
 
     l1.display_forward();
+    l1.erase(l1.begin(), l1.end());
+    l1.push_back(9);
+    l1.push_front(7);
+    
+    l1.display_forward();
+    l1.display_backward();
+
+
+    std::cout << l1.size() << std::endl;
+    
+
+    // l1.clear();
+
+    l1.display_forward();
+    l2.display_forward();
+    l2.display_backward();
 
 
     return 0;
--- list/list.h
+++ list/list.h
@@ -39,9 +39,12 @@ private:
         return new_node;
     }
 
-private:
+    using EdgeNodes = std::pair<Node *, Node *>;
+
+public:
     class BiDirectIterator
     {
+        friend class List;
         // * // Iterator traits
     public:
         using iterator_category = std::bidirectional_iterator_tag;
@@ -143,6 +146,7 @@ private:
         Node *sentinel;
     };
 
+    // * display
 public:
     void display_forward()
     {
@@ -166,6 +170,7 @@ public:
         std::cout << std::endl;
     }
 
+    // * Constructors
 public:
     List() : size_m{0}
     {
@@ -181,16 +186,7 @@ public:
             return;
         }
 
-        Node *head = create_new_node(value);
-        Node *tail = head;
-
-        for (size_t i = 0; i < count - 1; i++)
-        {
-            Node *new_node = create_new_node(value);
-            tail->next = new_node;
-            new_node->prev = tail;
-            tail = new_node;
-        }
+        auto [head, tail] = create_range(count, value);
 
         sentinel_m->next = head;
         sentinel_m->prev = tail;
@@ -198,8 +194,8 @@ public:
         tail->next = sentinel_m;
     }
 
-    template <class InputIt>
-    List(InputIt first, InputIt last)
+    template <typename InputIt>
+    List(InputIt first, InputIt last, typename std::enable_if<!std::is_integral<InputIt>::value>::type * = nullptr)
     {
         sentinel_m = create_new_sentiel();
         size_m = 0;
@@ -211,18 +207,7 @@ public:
             return;
         }
 
-        Node *head = create_new_node(*first++);
-        Node *tail = head;
-        ++size_m;
-
-        while (first != last)
-        {
-            Node *new_node = create_new_node(*first++);
-            tail->next = new_node;
-            new_node->prev = tail;
-            tail = new_node;
-            ++size_m;
-        }
+        auto [head, tail] = create_range(first, last);
 
         sentinel_m->next = head;
         sentinel_m->prev = tail;
@@ -239,6 +224,313 @@ public:
             return;
         }
 
+        copy_from_List(other);
+    }
+
+    List(List &&other) : size_m(other.size_m), sentinel_m(other.sentinel_m)
+    {
+        other.sentinel_m = create_new_sentiel();
+        other.size_m = 0;
+    }
+
+    List(std::initializer_list<T> init) : size_m{init.size()}
+    {
+        sentinel_m = create_new_sentiel();
+
+        if (size_m == 0)
+        {
+            return;
+        }
+
+        auto [head, tail] = create_range(init);
+
+        sentinel_m->next = head;
+        sentinel_m->prev = tail;
+        head->prev = sentinel_m;
+        tail->next = sentinel_m;
+    }
+
+    // * Destructor
+    ~List()
+    {
+        clear();
+        delete sentinel_m;
+    }
+
+    // * operator=
+    List &operator=(const List &other)
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+        clear();
+        delete sentinel_m;
+
+        size_m = other.size_m;
+        sentinel_m = create_new_sentiel();
+
+        if (size_m == 0)
+        {
+            return *this;
+        }
+
+        copy_from_List(other);
+
+        return *this;
+    }
+
+    List &operator=(List &&other)
+    {
+        if (this == &other)
+        {
+            return *this;
+        }
+
+        clear();
+        delete sentinel_m;
+
+        size_m = other.size_m;
+        sentinel_m = other.sentinel_m;
+
+        other.size_m = 0;
+        other.sentinel_m = create_new_sentiel();
+
+        return *this;
+    }
+
+    // * Element access
+public:
+    T &front()
+    {
+        if (sentinel_m->next == sentinel_m)
+        {
+            throw std::out_of_range("Cannot derefence value");
+        }
+        return sentinel_m->next->value;
+    }
+
+    T &back()
+    {
+        if (sentinel_m->prev == sentinel_m)
+        {
+            throw std::out_of_range("Cannot derefence value");
+        }
+        return sentinel_m->prev->value;
+    }
+
+    // *iterators
+public:
+    BiDirectIterator begin()
+    {
+        return BiDirectIterator(sentinel_m->next, sentinel_m);
+    }
+
+    BiDirectIterator end()
+    {
+        return BiDirectIterator(sentinel_m, sentinel_m);
+    }
+
+    // * Capacity
+public:
+    bool empty() const noexcept
+    {
+        return size_m == 0;
+    }
+
+    size_t size() const
+    {
+        return size_m;
+    }
+
+    // * Modifiers
+public:
+    void clear()
+    {
+        Node *current = sentinel_m->next;
+        while (current != sentinel_m)
+        {
+            Node *next = current->next;
+            delete current;
+            current = next;
+        }
+        sentinel_m->next = sentinel_m;
+        sentinel_m->prev = sentinel_m;
+        size_m = 0;
+    }
+
+    // * Insert
+    BiDirectIterator insert(BiDirectIterator pos, const T &value)
+    {
+        if (pos.node == nullptr)
+        {
+            return pos;
+        }
+
+        Node *new_node = create_new_node(value);
+        link_nodes(pos.node->prev, new_node, new_node, pos.node);
+        ++size_m;
+
+        return BiDirectIterator(new_node, sentinel_m);
+    }
+
+    BiDirectIterator insert(BiDirectIterator pos, size_t count, const T &value)
+    {
+        if (count == 0 || pos.node == nullptr)
+        {
+            return pos;
+        }
+
+        auto [head, tail] = create_range(count, value);
+        link_nodes(pos.node->prev, head, tail, pos.node);
+        size_m += count;
+
+        return BiDirectIterator(head, sentinel_m);
+    }
+
+    template <class InputIt>
+    BiDirectIterator insert(BiDirectIterator pos, InputIt first, InputIt last,
+                            typename std::enable_if<!std::is_integral<InputIt>::value>::type * = nullptr)
+    {
+        if (first == last || pos.node == nullptr)
+        {
+            return pos;
+        }
+
+        auto [head, tail] = create_range(first, last);
+        link_nodes(pos.node->prev, head, tail, pos.node);
+
+        return BiDirectIterator(head, sentinel_m);
+    }
+
+    BiDirectIterator insert(BiDirectIterator pos, std::initializer_list<T> ilist)
+    {
+        if (ilist.size() == 0 || pos.node == nullptr)
+        {
+            return pos;
+        }
+
+        auto [head, tail] = create_range(ilist);
+        link_nodes(pos.node->prev, head, tail, pos.node);
+        size_m += ilist.size();
+
+        return BiDirectIterator(head, sentinel_m);
+    }
+
+    // * erase
+    BiDirectIterator erase(BiDirectIterator pos)
+    {
+        if (pos.node == nullptr || pos == end())
+        {
+            throw std::invalid_argument("invalid pos");
+        }
+        Node *prev = pos.node->prev;
+        Node *next = pos.node->next;
+
+        prev->next = next;
+        next->prev = prev;
+        delete pos.node;
+
+        --size_m;
+
+        return BiDirectIterator(next, sentinel_m);
+    }
+
+    BiDirectIterator erase(BiDirectIterator first, BiDirectIterator last)
+    {
+        if (first == last)
+        {
+            return last;
+        }
+
+        Node *prev = first.node->prev;
+        Node *stop = last.node;
+
+        Node *current = first.node;
+        while (current != stop)
+        {
+            Node *next = current->next;
+            delete current;
+            current = next;
+            --size_m;
+        }
+
+        prev->next = stop;
+        stop->prev = prev;
+
+        return BiDirectIterator(stop, sentinel_m);
+    }
+
+    // * back methods
+    void push_back(const T &value)
+    {
+        Node *prev = sentinel_m->prev;
+        Node *new_node = create_new_node(value);
+
+        prev->next = new_node;
+        new_node->prev = prev;
+
+        sentinel_m->prev = new_node;
+        new_node->next = sentinel_m;
+
+        ++size_m;
+    }
+
+    void pop_back()
+    {
+        if (empty())
+        {
+            return;
+        }
+
+        Node *tail = sentinel_m->prev;
+        Node *prev = tail->prev;
+
+        delete tail;
+
+        prev->next = sentinel_m;
+        sentinel_m->prev = prev;
+
+        --size_m;
+    }
+
+    // * front methods
+
+    void push_front(const T &value)
+    {
+        Node *next = sentinel_m->next;
+        Node *new_node = create_new_node(value);
+
+        next->prev = new_node;
+        new_node->next = next;
+
+        sentinel_m->next = new_node;
+        new_node->prev = sentinel_m;
+
+        ++size_m;
+    }
+
+    void pop_front()
+    {
+        if (empty())
+        {
+            return;
+        }
+
+        Node *head = sentinel_m->next;
+        Node *next = head->next;
+
+        delete head;
+
+        sentinel_m->next = next;
+        next->prev = sentinel_m;
+
+        --size_m;
+    }
+
+private:
+    void copy_from_List(const List &other)
+    {
         Node *current = other.sentinel_m->next;
         Node *head = create_new_node(current->value);
         Node *tail = head;
@@ -259,21 +551,42 @@ public:
         tail->next = sentinel_m;
     }
 
-    List(List &&other) : size_m(other.size_m), sentinel_m(other.sentinel_m)
+    EdgeNodes create_range(size_t count, const T &value)
     {
-        other.sentinel_m = create_new_sentiel();
-        other.size_m = 0;
-    }
+        Node *head = create_new_node(value);
+        Node *tail = head;
 
-    List(std::initializer_list<T> init) : size_m{init.size()}
-    {
-        sentinel_m = create_new_sentiel();
-
-        if (size_m == 0)
+        for (size_t i = 0; i < count - 1; i++)
         {
-            return;
+            Node *new_node = create_new_node(value);
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
         }
 
+        return {head, tail};
+    }
+
+    template <typename InputIt>
+    EdgeNodes create_range(InputIt first, InputIt last, typename std::enable_if<!std::is_integral<InputIt>::value>::type * = nullptr)
+    {
+        Node *head = create_new_node(*first++);
+        Node *tail = head;
+        ++size_m;
+
+        while (first != last)
+        {
+            Node *new_node = create_new_node(*first++);
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
+            ++size_m;
+        }
+        return {head, tail};
+    }
+
+    EdgeNodes create_range(std::initializer_list<T> init)
+    {
         auto it = init.begin();
         Node *head = create_new_node(*it++);
         Node *tail = head;
@@ -285,46 +598,18 @@ public:
             new_node->prev = tail;
             tail = new_node;
         }
-
-        sentinel_m->next = head;
-        sentinel_m->prev = tail;
-        head->prev = sentinel_m;
-        tail->next = sentinel_m;
+        return {head, tail};
     }
 
-    ~List()
+    void link_nodes(Node *before, Node *head, Node *tail, Node *after)
     {
-        clear();
-        delete sentinel_m;
+        before->next = head;
+        head->prev = before;
+
+        tail->next = after;
+        after->prev = tail;
     }
 
-    void clear()
-    {
-        Node *current = sentinel_m->next;
-        while (current != sentinel_m)
-        {
-            Node *next = current->next;
-            delete current;
-            current = next;
-        }
-        sentinel_m->next = sentinel_m;
-        sentinel_m->prev = sentinel_m;
-        size_m = 0;
-    }
-
-    // *iterators
-
-    BiDirectIterator begin()
-    {
-        return BiDirectIterator(sentinel_m->next, sentinel_m);
-    }
-
-    BiDirectIterator end()
-    {
-        return BiDirectIterator(sentinel_m, sentinel_m);
-    }
-
-public:
 private:
     Node *sentinel_m;
     std::size_t size_m;

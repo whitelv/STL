642862de4e01acab869a566baba481263c402c66
white_lv <lubomyr0901@gmail.com>
2025-08-15 16:26:38 +0300

    .

---  Data Race & Race Condition/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liubomyr/Desktop/STL/ Data Race & Race Condition
-# Build directory: /home/liubomyr/Desktop/STL/ Data Race & Race Condition/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/liubomyr/Desktop/STL/ Data Race & Race Condition/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
---  Data Race & Race Condition/build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/liubomyr/Desktop/STL/ Data Race & Race Condition/build/tests[1]_tests.cmake")
-  include("/home/liubomyr/Desktop/STL/ Data Race & Race Condition/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
